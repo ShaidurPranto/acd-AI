8d21a070f4bc2b14f7de3907e8be3b67a18be8dc
ShaidurPranto <mdshaidurrahmanpranto@gmail.com>
2025-06-17 12:56:29 +0600

    f

--- offline3_Adversarial_search/main.cpp
+++ offline3_Adversarial_search/main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int main() {
-    int rows = 3;
-    int cols = 3;
-    Player player1 = { AI, RED, 1, 2 }; // mode, color, depth, heuristic_type(1-5) 
+    int rows = 9;
+    int cols = 6;
+    Player player1 = { AI, RED, 1, 1 }; // mode, color, depth, heuristic_type(1-5) 
     Player player2 = { AI, GREEN, 1, 5 };  // depth , heuristic_type is optional for HUMAN
     ChainReaction game(rows, cols, player1, player2, "game_state.txt");
     game.start();
